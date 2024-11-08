fa08d4566eb363008ff02fd65b29365cef9ced51
Kanyadetschool <oduorgeofrey141@gmail.com>
2024-11-08 06:13:11 +0300

    pushoo

--- Capture scores/Grade 1/Genre Popup.js
+++ Capture scores/Grade 1/Genre Popup.js
@@ -2,21 +2,27 @@ document.getElementById('search').addEventListener('click', function() {
   Swal.fire({
    
     html: `
-      <ul>
-      <li><a href="../Grade 2/Grade 2 Captures.html" target="_blank">Grade 2</a></li>
-      <li><a href="../Grade 3/Grade 3 Captures.html" target="_blank">Grade 3</a></li>
-      <li><a href="../Grade 4/Grade 4 Captures.html" target="_blank">Grade 4</a></li>
-      <li><a href="../Grade 5/Grade 5 Captures.html" target="_blank">Grade 5</a></li>
-      <li><a href="../Grade 6/Grade 6 Captures.html" target="_blank">Grade 6</a></li>
-      <li><a href="../Grade 7/Grade 7 Captures.html" target="_blank">Grade 7</a></li>
-      <li><a href="../Grade 8/Grade 8 Captures.html" target="_blank">Grade 8</a></li>
-      <li><a href="../Grade 9/Grade 9 Captures.html" target="_blank">Grade 9</a></li>
+     <img src="../images/icon-512x512.png" alt="" class="logoInswal">
+     	<img src="../images/right-arrow-svgrepo-com.svg" class="logoInswal">
+     <img src="../images/knec.png" alt="" class="logoInswal">
+    <h2>Select Class to proceed </h2> 
+    
+    <hr>
+
+      <div class="GenreClasses">
+      <a href="../Grade 2/Grade 2 Captures.html" target="_blank">Grade 2</a></li>
+      <a href="../Grade 3/Grade 3 Captures.html" target="_blank">Grade 3</a></li>
+      <a href="../Grade 4/Grade 4 Captures.html" target="_blank">Grade 4</a></li>
+      <a href="../Grade 5/Grade 5 Captures.html" target="_blank">Grade 5</a></li>
+      <a href="../Grade 6/Grade 6 Captures.html" target="_blank">Grade 6</a></li>
+      <a href="../Grade 7/Grade 7 Captures.html" target="_blank">Grade 7</a></li>
+      <a href="../Grade 8/Grade 8 Captures.html" target="_blank">Grade 8</a></li>
+      <a href="../Grade 9/Grade 9 Captures.html" target="_blank">Grade 9</a></li>
        
-      </ul>
+      </div>
     `,
     showCloseButton: true,
     showConfirmButton: false,
-    width: '400px',
     padding: '1em',
     customClass: {
       container: 'swal-container',
--- Capture scores/Grade 9/Genre Popup.js
+++ Capture scores/Grade 9/Genre Popup.js
@@ -1,29 +1,42 @@
 
-  document.getElementById('search').addEventListener('click', function() {
-    Swal.fire({
-     
-      html: `
-        <ul>
-          <li><a href="../Grade 1/Grade 1 Captures.html" target="_blank">Grade 1</a></li>
-          <li><a href="../Grade 2/Grade 2 Captures.html" target="_blank">Grade 2</a></li>
-          <li><a href="../Grade 3/Grade 3 Captures.html" target="_blank">Grade 3</a></li>
-          <li><a href="../Grade 4/Grade 4 Captures.html" target="_blank">Grade 4</a></li>
-          <li><a href="../Grade 5/Grade 5 Captures.html" target="_blank">Grade 5</a></li>
-          <li><a href="../Grade 6/Grade 6 Captures.html" target="_blank">Grade 6</a></li>
-          <li><a href="../Grade 7/Grade 7 Captures.html" target="_blank">Grade 7</a></li>
-          <li><a href="../Grade 8/Grade 8 Captures.html" target="_blank">Grade 8</a></li>
-         
-        </ul>
-      `,
-      showCloseButton: true,
-      showConfirmButton: false,
-      width: '400px',
-      padding: '1em',
-      customClass: {
-        container: 'swal-container',
-        title: 'swal-title',
-        htmlContainer: 'swal-html-container'
-      }
-    });
+document.getElementById('search').addEventListener('click', function() {
+  Swal.fire({
+   
+    html: `
+   
+     <img src="../images/icon-512x512.png" alt="" class="logoInswal">
+     	<img src="../images/right-arrow-svgrepo-com.svg" class="logoInswal">
+     <img src="../images/knec.png" alt="" class="logoInswal">
+    <h2>Select Class to proceed </h2> 
+    
+    <hr>
+
+      <div class="GenreClasses">
+
+        <a href="../Grade 1/Grade 1 Captures.html" target="_blank">Grade 1</a></li>
+        <a href="../Grade 2/Grade 2 Captures.html" target="_blank">Grade 2</a></li>
+        <a href="../Grade 3/Grade 3 Captures.html" target="_blank">Grade 3</a></li>
+        <a href="../Grade 4/Grade 4 Captures.html" target="_blank">Grade 4</a></li>
+        <a href="../Grade 5/Grade 5 Captures.html" target="_blank">Grade 5</a></li>
+        <a href="../Grade 6/Grade 6 Captures.html" target="_blank">Grade 6</a></li>
+        <a href="../Grade 7/Grade 7 Captures.html" target="_blank">Grade 7</a></li>
+        <a href="../Grade 8/Grade 8 Captures.html" target="_blank">Grade 8</a></li>
+       
+      </div>
+    `,
+    showCloseButton: true,
+    showConfirmButton: false,
+    padding: '1em',
+    customClass: {
+      container: 'swal-container',
+      title: 'swal-title',
+      htmlContainer: 'swal-html-container'
+    }
   });
+});
+
+
+
+
+
 
