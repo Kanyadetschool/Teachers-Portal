814556b7dd3ee8336a7027ce5edaa80f93bbc7c3
Kanyadetschool <oduorgeofrey141@gmail.com>
2025-03-05 06:44:03 +0300

    push main

--- js/ImagePopup.js
+++ js/ImagePopup.js
@@ -3,20 +3,19 @@ document.getElementById('Ambrose').addEventListener('click', function() {
   Swal.fire({
    
     html: `
-    <h2>MR AMBROSE OUMA OKELLO - HOI</h2> 
+    <h2>MR AMBROSE OUMA ODHIAMBO - HOI</h2> 
       <div class="GenreClasses">
-    <img src="images/anode.png" alt="brain"> <br>
-     Current HeadTeacher <br>
-     Entry 6th Jan 2025 - <br>
-     TSC NO:29009 <br>
-     Marital status :Married <br>
-     Gender:Male <br>
+    <img src="images/ambrose.png" alt="brain"> <br>
+    Current HeadTeacher <br>
+    Entry 6th Jan 2025 - <br>
+    TSC NO:29009 <br>
+    Marital status :Married <br>
+    Gender:Male <br>
+    Ethnicity: Luo <br>
      Disability: None
-     <a href="tel:0729977321">Saf: 0729977321</a>
-     <a href="tel:07340945491">Airtel: 0734945491</a>
+     <a href="tel:0725657618">Saf: 0725657618</a>
      
      <a href="mailto:everlynatieno70@gmail.com" target="_blank">Send an Email</a>
-     <a href="./Profesional Documents/Sign In/Sign in.html" target="_blank">Professional Docs</a>
 
      
        
@@ -42,10 +41,11 @@ document.getElementById('FormerStaff').addEventListener('click', function() {
       <div class="GenreClasses">
     <img src="images/anode.png" alt="brain"> <br>
     MD Evelyn Anode Panyako <br>
-     Former HeadTeacher <br>
-     Entry 2022 - 2025 <br>
-     TSC NO:29009 <br>
-     Gender:Female <br>
+    Former HeadTeacher <br>
+    Entry 2022 - 2025 <br>
+    TSC NO:29009 <br>
+    Gender:Female <br>
+    Ethnicity: Luo <br>
      Disability: None
      :everlynatieno70@gmail.com
 
@@ -54,20 +54,22 @@ document.getElementById('FormerStaff').addEventListener('click', function() {
      
 
      <img src="images/mrokumu.png" alt="brain"> <br>
-    The late Mr Edwin Okumu<br>
+     The late Mr Edwin Okumu<br>
      Regular Teacher <br>
      Entry 2022 - 2023 <br>
      TSC NO:N/A <br>
      Gender:Male <br>
+     Ethnicity: Luo <br>
      <a href="#"></a>
      <a href="#"></a>
 
      <img src="images/schoologo.png" alt="brain"> <br>
      Mr cleophas Alwala<br>
-      Former Deputy HT <br>
+     Former Deputy HT <br>
      EXIT:2024 <br>
      TSC NO:N/A <br>
      Gender:Male <br>
+     Ethnicity: Luo <br>
        
       </div>
     `,
@@ -87,15 +89,15 @@ document.getElementById('Obure').addEventListener('click', function() {
   Swal.fire({
    
     html: `
-    <h2>MR FREDRICK GHANA OBURE</h2> 
+    <h2>MR FREDRICK GHANA OBURE - Senior Teacher</h2> 
       <div class="GenreClasses">
     <img src="images/obure-removebg-preview (2).png" alt="brain"> <br>
-     Status:Active <br>
-     Senior Teacher <br>
-     Entry: 2022  <br>
-     Exit: <br>
-     TSC NO: 261952 <br>
-     Marital status :Married <br>
+    Senior Teacher <br>
+    Entry: 2022  <br>
+    Exit: <br>
+    TSC NO: 261952 <br>
+    Marital status :Married <br>
+    Ethnicity: Luo <br>
      Gender:Male <br>
      Disability: None <br>
      <a href="tel:0720264777">Saf: 0720264777</a>
@@ -133,15 +135,15 @@ document.getElementById('Owuor').addEventListener('click', function() {
      Marital status :Married <br>
      Gender:Male <br>
      Disability: None <br>
-     Entry: ............  <br>
-     Exit: ............  <br>
+     Entry: 2000<br>
+     Exit:   <br>
      <a href="tel:0799783950">Saf: 0799783950</a>
      <a href="tel:0789512799">Airtel: 0789512799</a>
-     <a href="mailto:" target="_blank">Send an Email</a>
+     <a href="mailto:"wycliffjerry88@gmail.com" target="_blank"> Send an Email </a>
      <a href="./Profesional Documents/Sign In/Sign in.html" target="_blank">Professional Docs</a>
 
-       
-      </div>
+    
+    </div>
     `,
     showCloseButton: true,
     showConfirmButton: false,
@@ -158,7 +160,7 @@ document.getElementById('Owino').addEventListener('click', function() {
   Swal.fire({
    
     html: `
-    <h2>MR WYCLIFF OWINO</h2> 
+    <h2>MR WYCLIFF OWINO OPIYO</h2> 
       <div class="GenreClasses">
     <img src="images/Owino.png" alt="brain"> 
     <br>
@@ -191,6 +193,86 @@ document.getElementById('Owino').addEventListener('click', function() {
     }
   });
 });
+
+document.getElementById('lichuma').addEventListener('click', function() {
+  Swal.fire({
+   
+    html: `
+    <h2>MR VINCENT OLUBWAYO LICHUMA</h2> 
+      <div class="GenreClasses">
+    <img src="images/lichuma.jpg" alt="brain"> 
+  
+    Status:Active <br>
+     Regular Teacher <br>
+     Entry: 2021  <br>
+     TSC NO: 585940  <br>
+     Marital status :Married <br>
+     Gender:Male <br>
+     Disability: None <br>
+     <a href="tel:0751375060">Saf: 0751375060</a>
+     <a href="tel:078099518">Saf 2: 078099518</a>
+     <a href="mailto:" target="_blank">Send an Email</a>
+
+     
+     <a href="./Profesional Documents/Sign In/Sign in.html" target="_blank">Professional Docs</a>
+
+     
+     
+       
+      </div>
+    `,
+    showCloseButton: true,
+    showConfirmButton: false,
+    padding: '1em',
+    customClass: {
+      container: 'swal-container',
+      title: 'swal-title',
+      htmlContainer: 'swal-html-container'
+    }
+  });
+});
+
+
+
+document.getElementById('janet').addEventListener('click', function() {
+  Swal.fire({
+   
+    html: `
+    <h2>MD JANET ODHIAMBO OLUOCH</h2> 
+      <div class="GenreClasses">
+    <img src="images/mdjanet.png" alt="brain"> 
+  
+     Regular Teacher <br>
+     Entry: 2022  <br>
+     TSC NO: 801669  <br>
+     Marital status :Married <br>
+     Gender:Female <br>
+     Ethnicity: Luo <br>
+     Disability: None <br>
+     <a href="tel:0751375060">Saf: 0704258167</a>
+     <a href="tel:078099518">Saf 2: 0727691023</a>
+     <a href="mailto:" target="_blank">Send an Email</a>
+
+     
+     <a href="./Profesional Documents/Sign In/Sign in.html" target="_blank">Professional Docs</a>
+
+     
+     
+       
+      </div>
+    `,
+    showCloseButton: true,
+    showConfirmButton: false,
+    padding: '1em',
+    customClass: {
+      container: 'swal-container',
+      title: 'swal-title',
+      htmlContainer: 'swal-html-container'
+    }
+  });
+});
+
+
 
 document.getElementById('oduor').addEventListener('click', function() {
   Swal.fire({
